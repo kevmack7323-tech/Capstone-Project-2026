684a55ab4a29b6de05e7aa6e3f6788d78e6afa82
kevmack7323-tech <kevmack7323@gmail.com>
2026-08-03 17:08:58 -0400

    Created a schema model

---
+++ backend/models/Incident.js
@@ -0,0 +1,13 @@
+import mongoose from 'mongoose';
+
+const incidentSchema = new mongoose.Schema({
+    title: { type: String, required: true },
+    operationContext: { type: String, required: true },
+    ai_risk_level: { type: String, enum: ['Low','Medium','High','Critical'], required: true },
+    status: { type: String, default: 'Active' },
+    description: String
+}, { timestamps: true });
+
+const Incident = mongoose.model('Incident', incidentSchema);
+
+export default Incident;
