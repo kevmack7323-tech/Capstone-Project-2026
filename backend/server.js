e3d115a7549d062558df1b48645db2c03056b8ec
kevmack7323-tech <kevmack7323@gmail.com>
2026-08-05 14:47:17 -0400

    enabled cors to allow frontend/backend communication

--- backend/server.js
+++ backend/server.js
@@ -2,12 +2,14 @@ import express from "express";
 import connectDB from "./config/db.js";
 import dotenv, { configDotenv } from "dotenv";
 import incidentRoutes from "./routes/incidentRoutes.js";
+import cors from "cors";
 
 configDotenv()
 
 const app = express();
 
 app.use(express.json());
+app.use(cors());
 app.use("/api/incidents", incidentRoutes);
 
 app.get("/", (req, res) => {
