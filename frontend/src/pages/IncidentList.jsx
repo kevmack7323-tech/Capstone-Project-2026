f740b18187e45923d7524d65d5d6c54de1a82123
kevmack7323-tech <kevmack7323@gmail.com>
2026-08-05 14:42:23 -0400

    completed incidentList page

--- frontend/src/pages/IncidentList.jsx
+++ frontend/src/pages/IncidentList.jsx
@@ -1,6 +1,36 @@
 import api from "../api/axios.js"
+import { useEffect, useState } from "react"
 
+export default function IncidentList() {
 
-export default function IncidentList(){
+    const [incidents, setIncidents] = useState([]);
 
+    useEffect(() => {
+        const fetchIncidents = async () => {
+            try {
+                const result = await api.get("/incidents");
+                setIncidents(result.data);
+            } catch (error) {
+                console.log("Error fetching incidents:", error)
+            }
+        };
+
+        fetchIncidents();
+    }, []);
+
+    return (
+        <div>
+            <h1>MoMA Security Incidents</h1>
+
+            {incidents.map(incident => (
+                <div key={incident._id}>
+                    <h2>{incident.title}</h2>
+                    <p>{incident.description}</p>
+                    <p><strong>Severity:</strong> {incident.severity}</p>
+                    <p><strong>AI Risk:</strong> {incident.ai_risk_level}</p>
+                    <p><strong>Context:</strong> {incident.operationContext}</p>
+                </div>
+            ))}
+        </div>
+    );
 }
