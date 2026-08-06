3435d1d7ec3a73190dde3fb3a6c14cbd4c8171c1
kevmack7323-tech <kevmack7323@gmail.com>
2026-08-06 01:05:43 -0400

    Added nav for home and create incident

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -10,6 +10,7 @@ function App() {
 
   return (
     <Router>
+      <Navbar/>
       <Routes>
         <Route path="/" element={<IncidentList />} />
         <Route path="/create" element={<CreateIncident />} />
