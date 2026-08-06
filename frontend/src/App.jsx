e4c85008e6c7b29513afe8312b8941b0eb9b4d5e
kevmack7323-tech <kevmack7323@gmail.com>
2026-08-05 23:45:11 -0400

    Updated return in App.jsx for page navigation

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -2,14 +2,18 @@ import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import { useState } from 'react'
 import './App.css'
 import IncidentList from './pages/IncidentList'
+import CreateIncident from './pages/CreateIncident';
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
-    <>
-     <IncidentList/>
-    </>
+    <Router>
+      <Routes>
+        <Route path="/" element={<IncidentList />} />
+        <Route path="/create" element={<CreateIncident />} />
+      </Routes>
+    </Router>
   )
 }
 
