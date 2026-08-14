04512698efd72d46109b7908ecaedc622210010c
kevmack7323-tech <kevmack7323@gmail.com>
2026-08-14 13:16:09 -0400

    refactored schema to add AI summary and recommendations to Incident model

--- backend/models/Incident.js
+++ backend/models/Incident.js
@@ -2,11 +2,33 @@ import mongoose from 'mongoose';
 //Mongoose Schema defining the structure of an incident in document
 const incidentSchema = new mongoose.Schema({
     title: { type: String, required: true },
-    operationContext: { type: String, required: true },
-    severity: { type: String, required: true, enum: ["Low", "Medium", "High", "Critical"], default: "Low" },
-    ai_risk_level: { type: String, enum: ['Low', 'Medium', 'High', 'Critical'], required: true },
-    status: { type: String, default: 'Active' },
-    description: String
+    description: { type: String, required: true },
+    location: { type: String, required: true },
+
+    // User-selected physical severity
+    severity: {
+        type: String,
+        enum: ["Low", "Medium", "High", "Critical"],
+        default: "Low",
+        required: true
+    },
+
+    // calculated by ai
+    ai_risk_level: {
+        type: String,
+        enum: ['Low', 'Medium', 'High', 'Critical'],
+        default: "Low"
+    },
+
+    // Automated Ai Fields
+    aiSummary: { type: String },
+    tacticalRecommendations: [{ type: String }],
+
+    status: {
+        type: String,
+        enum: ["Open", "In Progress", "Resolved"],
+        default: "Open"
+    },
 }, { timestamps: true });
 
 const Incident = mongoose.model('Incident', incidentSchema);
