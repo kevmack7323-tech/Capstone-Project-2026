5948f45761a033cd36bb560f1b715ef3a912a70e
kevmack7323-tech <kevmack7323@gmail.com>
2026-08-14 21:45:23 -0400

    created User schema with paswword hashing middleware

---
+++ backend/models/User.js
@@ -0,0 +1,57 @@
+import mongoose from "mongoose";
+import bcrypt from "bcryptjs";
+
+const userSchema = new mongoose.Schema(
+    {
+        name: {
+            type: String,
+            required: [true, "Please add a name"],
+            trim: true,
+        },
+        email: {
+            type: String,
+            required: [true, "Please add a email"],
+            unique: true,
+            lowercase: true,
+            trim: true,
+            match: [
+                /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/,
+                "Please add a valid email",
+            ]
+        },
+        password: {
+            type: String,
+            required: [true, "Please add a password"],
+            minlength: 6,
+            select: false,
+        },
+        role: {
+            type: String,
+            enum: ["Officer", "Admin"],
+            default: "Officer",
+        },
+    },
+    {
+        timestamps: true,
+    }
+);
+
+// Pre-save middleware to hash password automatically before saving
+userSchema.pre("save", async function (next) {
+    if(!this.isModified("password")) {
+        return next();
+    }
+
+    const salt = await bcrypt.genSalt(10);
+    this.password = await bcrypt.hash(this.password, salt);
+    next();
+});
+
+// Helper method to compare entered passwor w/ stored hashed password
+userSchema.methods.matchPassword = async function (enteredPassword) {
+    return await bcrypt.compare(enteredPassword, this.password);
+};
+
+const User = mongoose.model("User", userSchema);
+
+export default User;
